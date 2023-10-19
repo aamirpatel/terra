322954ecab483868a01644c268e4f020f305abba
Aamir Patel <paamir89@gmail.com>
2023-10-20 04:16:56 +0530

    Update import.tf

--- import.tf
+++ import.tf
@@ -5,4 +5,8 @@ resource "azurerm_resource_group" "example" {
 
 provider "azurerm" {
     features {}
+subscription_id = "853976da-1943-482f-be67-2d729c61ef50"
+  client_id       = "a23248da-6072-4ed5-bd7c-2744a4e5f740"
+  client_secret   = "Uul8Q~2uZvcFgKDO-KQ3r3qzISklzhFnDTA.hcmL"
+  tenant_id       = "e8ac314e-8e13-4ff3-ad6c-ccba99ae326d"
 }
