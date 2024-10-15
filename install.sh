e0023c8ce1b9f5bc05e21c677658dc211687d18d
Ubuntu <ubuntu@testvm.lxued4wa41je3nakvqf0rfe0cd.rx.internal.cloudapp.net>
2024-10-15 06:44:02 +0000

    first commit

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+sudo apt-get update -y
+sudo apt-get install ca-certificates curl -y
+sudo install -m 0755 -d /etc/apt/keyrings -y
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+#installing java
+sudo apt install default-jre -y
+#installing Jenkins
+#sudo wget -O /usr/share/keyrings/jenkins-keyring.asc   https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+#echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]"   https://pkg.jenkins.io/debian-stable binary/ | sudo tee   /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update -y
+#sudo apt-get install jenkins -y
+#sudo systemctl enable jenkins -y
+#sudo usermod -aG docker jenkins
+sudo newgrp docker
+#installing minikube
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
+minikube start  --driver=docker
+alias kubectl="minikube kubectl --"
