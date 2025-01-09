f5b7b4074d9c1c60a6ecd2bbacc96c9f2d488f7b
Aamir Patel <paamir89@gmail.com>
2025-01-09 13:35:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,7 +19,14 @@ provider "aws" {
 
 resource "aws_s3_bucket" "example" {
   bucket = "my-tf-test-bucket7112081"
-  encrypt = "true"
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"  # AES-256 is the encryption algorithm for SSE-S3
+      }
+    }
+  }
 
   tags = {
     Name        = "My bucket"
