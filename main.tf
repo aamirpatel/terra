dadfae7878296040eecb4250b805dfa0e00f6b2e
Aamir Patel <paamir89@gmail.com>
2025-01-09 13:38:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,19 +17,24 @@ provider "aws" {
   }
 } */
 
-resource "aws_s3_bucket" "example" {
+
+
+resource "aws_kms_key" "mykey" {
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 10
+}
+
+resource "aws_s3_bucket" "mybucket" {
   bucket = "my-tf-test-bucket7112081"
+}
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"  # AES-256 is the encryption algorithm for SSE-S3
-      }
+resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
+  bucket = aws_s3_bucket.mybucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.mykey.arn
+      sse_algorithm     = "aws:kms"
     }
-  }
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
   }
 }
