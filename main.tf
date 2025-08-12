cc60879af839ff75e028c5fca805cde28530473a
Aamir Patel <paamir89@gmail.com>
2025-08-12 16:43:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ resource "aws_s3_bucket" "mybucket" {
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
-  bucket = aws_s3_bucket.mybucket.id
+  bucket = aws_s3_bucket.example.id
 
   rule {
     apply_server_side_encryption_by_default {
